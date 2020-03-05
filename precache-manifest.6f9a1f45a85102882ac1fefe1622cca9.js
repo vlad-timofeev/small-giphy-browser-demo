0e17d506d27b9bd68c7d7bf4bc611a9f6741fc2f
Vladislav Timofeev <vtimofeev.m@gmail.com>
2020-03-06 00:37:37 +0700

    update files

---
+++ precache-manifest.6f9a1f45a85102882ac1fefe1622cca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe115b71061135fdaef0ebb1e20cc37",
+    "url": "/small-giphy-browser-demo/index.html"
+  },
+  {
+    "revision": "c7ac4c03246096668680",
+    "url": "/small-giphy-browser-demo/static/css/main.3ef27516.chunk.css"
+  },
+  {
+    "revision": "4ed5ba4e1b935621f99a",
+    "url": "/small-giphy-browser-demo/static/js/2.8467e8fa.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/small-giphy-browser-demo/static/js/2.8467e8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ac4c03246096668680",
+    "url": "/small-giphy-browser-demo/static/js/main.199e364c.chunk.js"
+  },
+  {
+    "revision": "e163aaf0f22696d20cab",
+    "url": "/small-giphy-browser-demo/static/js/runtime-main.17844363.js"
+  }
+]);
